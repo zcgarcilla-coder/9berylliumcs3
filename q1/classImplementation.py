5d405a67b2ba14633f7b2846ff13c498f8fc45a3
zcgarcilla <zcgarcilla@brc.pshs.edu.ph>
2026-09-08 12:23:13 +0800

    Update classImplementation.py

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -1,1 +1,7 @@
+class Movies:
+def __init__(movie, title, director, genre, rating):
+  movie.attribute1 = title
+  movie.attribute2 = director
+  movie.attribute3 = genre
+  movie.__private_attribute = rating
 
