5edb8d306c0b510c592d0c9319f796a2760c36bd
zcgarcilla-coder <zcgarcilla@brc.pshs.edu.ph>
2026-09-08 21:05:33 +0800

    modified class implementation

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -5,8 +5,67 @@ class Movies:
         movie.attribute3 = genre
         movie.__private_attribute = rating
 
+def greet(name):
+    print(f"Hello, {name}! Welcome to the Movie Inventory System.")
+    print("Welcome to the movie inventory system!")
 
+greet("User")
+  
 inventory = []
+
+def add_movie():
+  title = (input("Enter the title of the movie: "))
+  director = (input("Enter the director of the movie: "))
+  genre = (input("Enter the genre of the movie: "))
+  rating = (input("Enter the rating of the movie: "))
+  movie = Movies(title, director, genre, rating)
+  inventory.append(movie)
+  return inventory
+    
+def display_movies():
+  if len(inventory) == 0:
+    print("No movies in the inventory.")
+  else:
+    for i, movie in enumerate(inventory):
+      print(f"\nMovie {i+1}:")
+      print(f"Title: {movie.attribute1}")
+      print(f"Director: {movie.attribute2}")
+      print(f"Genre: {movie.attribute3}")
+      print(f"Rating: {movie._Movies__private_attribute}")
+  return inventory
+        
+def update_movie():
+  if len(inventory) == 0:
+    print("No movies in the inventory.")
+  else:
+    display_movies()
+    movie_index = int(input("Enter the number of the movie you want to update: ")) - 1
+    if movie_index < 0 or movie_index >= len(inventory):
+      print("Invalid index. Please try again.")
+    else:
+      title = (input("Enter the new title of the movie: "))
+      director = (input("Enter the new director of the movie: "))
+      genre = (input("Enter the new genre of the movie: "))
+      rating = (input("Enter the new rating of the movie: "))
+      inventory[movie_index].attribute1 = title
+      inventory[movie_index].attribute2 = director
+      inventory[movie_index].attribute3 = genre
+      inventory[movie_index]._Movies__private_attribute = rating
+      print("Movie updated successfully.")
+  return inventory
+
+def delete_movie():
+  if len(inventory) == 0:
+    print("No movies in the inventory.")
+  else:
+    display_movies()
+    movie_index = int(input("Enter the number of the movie you want to delete: ")) - 1
+    if movie_index < 0 or movie_index >= len(inventory):
+      print("Invalid index. Please try again.")
+    else:
+      del inventory[movie_index]
+      print("Movie deleted successfully.")
+  return inventory
 
 while True:
   print("\n1. Add movie.")
@@ -18,12 +77,7 @@ while True:
   choice = int(input("Enter your choice (*must be only the number): "))
 
   if choice == 1:
-    title = (input("Enter the title of the movie: "))
-    director = (input("Enter the director of the movie: "))
-    genre = (input("Enter the genre of the movie: "))
-    rating = (input("Enter the rating of the movie: "))
-    movie = Movies(title, director, genre, rating)
-    inventory.append(movie)
+    add_movie()
   elif choice == 2:
     display_movies()
   elif choice == 3:
@@ -34,53 +88,3 @@ while True:
     break
   else:
     print("Invalid choice. Please try again.")
-    
-  def add_movies():
-    title = (input("Enter the title of the movie: "))
-    director = (input("Enter the director of the movie: "))
-    genre = (input("Enter the genre of the movie: "))
-    rating = (input("Enter the rating of the movie: "))
-    movie = Movies(title, director, genre, rating)
-    inventory.append(movie)
-    
-  def display_movies():
-    if len(inventory) == 0:
-      print("No movies in the inventory.")
-    else:
-      for i, movie in enumerate(inventory):
-        print(f"\nMovie {i+1}:")
-        print(f"Title: {movie.attribute1}")
-        print(f"Director: {movie.attribute2}")
-        print(f"Genre: {movie.attribute3}")
-        print(f"Rating: {movie._Movies__private_attribute}")
-        
-  def update_movie():
-    if len(inventory) == 0:
-      print("No movies in the inventory.")
-    else:
-      display_movies()
-      movie_index = int(input("Enter the index of the movie you want to update: ")) - 1
-      if movie_index < 0 or movie_index >= len(inventory):
-        print("Invalid index. Please try again.")
-      else:
-        title = (input("Enter the new title of the movie: "))
-        director = (input("Enter the new director of the movie: "))
-        genre = (input("Enter the new genre of the movie: "))
-        rating = (input("Enter the new rating of the movie: "))
-        inventory[movie_index].attribute1 = title
-        inventory[movie_index].attribute2 = director
-        inventory[movie_index].attribute3 = genre
-        inventory[movie_index]._Movies__private_attribute = rating
-        print("Movie updated successfully.")
-        
-  print("--- BEFORE ---")
-  object1 = Movies("Inception", "Christopher Nolan", "Sci-Fi", "8.8")
-  object2 = Movies("The Shawshank Redemption", "Frank Darabont", "Drama", "9")
-
-  print(f"Object 1: {object1.attribute1}, {object1.attribute2}, {object1.attribute3}, {object1._Movies__private_attribute}")
-  print(f"Object 2: {object2.attribute1}, {object2.attribute2}, {object2.attribute3}, {object2._Movies__private_attribute}")
-
-  object1.update_movie("Inception", "Christopher Nolan", "Sci-Fi", "9")
-  print("--- AFTER ---")
-  print(f"Object 1: {object1.attribute1}, {object1.attribute2}, {object1.attribute3}, {object1._Movies__private_attribute}")
-  print(f"Object 2: {object2.attribute1}, {object2.attribute2}, {object2.attribute3}, {object2._Movies__private_attribute}")
